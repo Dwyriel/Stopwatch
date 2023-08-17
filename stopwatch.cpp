9e536293038df2cd0b4e1b62ce30c9e40afc79c2
Dwyriel <dwyriel333@hotmail.com>
2023-08-17 15:11:55 -0300

    Stopwatch steady_clock implementation

--- stopwatch.cpp
+++ stopwatch.cpp
@@ -6,6 +6,14 @@ std::chrono::time_point<std::chrono::high_resolution_clock> Stopwatch::m_start =
 
 std::chrono::time_point<std::chrono::high_resolution_clock> Stopwatch::m_end = std::chrono::high_resolution_clock::now();
 
+template<class Ratio>
+double Stopwatch::elapsedByPrecision() {
+    if (isRunning)
+        m_end = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double, Ratio> elapsed = m_end - m_start;
+    return elapsed.count();
+}
+
 void Stopwatch::start() {
     isRunning = true;
     m_start = std::chrono::high_resolution_clock::now();
@@ -17,36 +25,65 @@ void Stopwatch::stop() {
 }
 
 double Stopwatch::elapsedPicoseconds() {
-    if (isRunning)
-        m_end = std::chrono::high_resolution_clock::now();
-    std::chrono::duration<double, std::pico> elapsed = m_end - m_start;
-    return elapsed.count();
+    return elapsedByPrecision<std::pico>();
 }
 
 double Stopwatch::elapsedNanoseconds() {
-    if (isRunning)
-        m_end = std::chrono::high_resolution_clock::now();
-    std::chrono::duration<double, std::nano> elapsed = m_end - m_start;
-    return elapsed.count();
+    return elapsedByPrecision<std::nano>();
 }
 
 double Stopwatch::elapsedMicroseconds() {
-    if (isRunning)
-        m_end = std::chrono::high_resolution_clock::now();
-    std::chrono::duration<double, std::micro> elapsed = m_end - m_start;
-    return elapsed.count();
+    return elapsedByPrecision<std::micro>();
 }
 
 double Stopwatch::elapsedMilliseconds() {
-    if (isRunning)
-        m_end = std::chrono::high_resolution_clock::now();
-    std::chrono::duration<double, std::milli> elapsed = m_end - m_start;
-    return elapsed.count();
+    return elapsedByPrecision<std::milli>();
 }
 
 double Stopwatch::elapsedSeconds() {
+    return elapsedByPrecision<std::ratio<1>>();
+}
+
+bool StopwatchSC::isRunning = false;
+
+std::chrono::time_point<std::chrono::steady_clock> StopwatchSC::m_start = std::chrono::steady_clock::now();
+
+std::chrono::time_point<std::chrono::steady_clock> StopwatchSC::m_end = std::chrono::steady_clock::now();
+
+template<class Ratio>
+double StopwatchSC::elapsedByPrecision() {
     if (isRunning)
-        m_end = std::chrono::high_resolution_clock::now();
-    std::chrono::duration<double> elapsed = m_end - m_start;
+        m_end = std::chrono::steady_clock::now();
+    std::chrono::duration<double, Ratio> elapsed = m_end - m_start;
     return elapsed.count();
+}
+
+void StopwatchSC::start() {
+    isRunning = true;
+    m_start = std::chrono::steady_clock::now();
+}
+
+void StopwatchSC::stop() {
+    m_end = std::chrono::steady_clock::now();
+    isRunning = false;
+}
+
+double StopwatchSC::elapsedPicoseconds() {
+    return elapsedByPrecision<std::pico>();
+}
+
+double StopwatchSC::elapsedNanoseconds() {
+    return elapsedByPrecision<std::nano>();
+}
+
+double StopwatchSC::elapsedMicroseconds() {
+    return elapsedByPrecision<std::micro>();
+}
+
+double StopwatchSC::elapsedMilliseconds() {
+    return elapsedByPrecision<std::milli>();
+}
+
+double StopwatchSC::elapsedSeconds() {
+    return elapsedByPrecision<std::ratio<1>>();
 }
--- stopwatch.h
+++ stopwatch.h
@@ -3,12 +3,51 @@
 
 #include <chrono>
 
+/**
+ * <br>Default Stopwatch, uses std::chrono::high_resolution_clock
+ */
 class Stopwatch {
     static bool isRunning;
     static std::chrono::time_point<std::chrono::high_resolution_clock> m_start, m_end;
 
+    template<class Ratio>
+    static inline double elapsedByPrecision();
+
 public:
     Stopwatch() = delete;
+
+    static void start();
+
+    static void stop();
+
+    static double elapsedPicoseconds();
+
+    static double elapsedNanoseconds();
+
+    static double elapsedMicroseconds();
+
+    static double elapsedMilliseconds();
+
+    static double elapsedSeconds();
+};
+
+/**
+ * <br>Uses std::chrono::high_resolution_clock
+ */
+using StopwatchHRC = Stopwatch;
+
+/**
+ * <br>Uses std::chrono::steady_clock
+ */
+class StopwatchSC {
+    static bool isRunning;
+    static std::chrono::time_point<std::chrono::steady_clock> m_start, m_end;
+
+    template<class Ratio>
+    static inline double elapsedByPrecision();
+
+public:
+    StopwatchSC() = delete;
 
     static void start();
 
